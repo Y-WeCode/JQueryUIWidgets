432f08ed43569daf385ec3251c9ac94e5ce43cff
Raphael <rbabel@yes-we-code.fr>
2015-11-27 16:49:50 +0100

    Slider + Autocomplete

---
+++ Form/Type/AutocompleteType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace YWC\Common\JQueryUIWidgetsBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Exception\LogicException;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AutocompleteType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'ywc_autocomplete_type' => null,
+            'text_value' => null,
+        ));
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        if(is_null($options['ywc_autocomplete_type'])) {
+            throw new LogicException('Autocomplete field option "ywc_autocomplete_type" cannot be null.');
+        }
+
+        $view->vars = array_replace($view->vars, array(
+            'ywc_autocomplete_type'  => $options['ywc_autocomplete_type'],
+            'text_value' => $options['text_value'],
+        ));
+    }
+
+    public function getParent()
+    {
+        return 'text';
+    }
+
+    public function getName()
+    {
+        return 'ywc_autocomplete';
+    }
+}
---
+++ Form/Type/SliderType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace YWC\Common\JQueryUIWidgetsBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SliderType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'ywc_max' => 100,
+            'ywc_min' => 0,
+            'empty_data' => 0,
+        ));
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars = array_replace($view->vars, array(
+            'ywc_max'  => $options['ywc_max'],
+            'ywc_min'  => $options['ywc_min'],
+        ));
+    }
+
+    public function getParent()
+    {
+        return 'text';
+    }
+
+    public function getName()
+    {
+        return 'ywc_slider';
+    }
+}
