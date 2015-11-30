304c45dbef85db9fd8bc907d79a6164044c7f638
Raphael <rbabel@yes-we-code.fr>
2015-11-30 10:56:49 +0100

    cgu widget

---
+++ Form/Type/CGUType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace YWC\UI\CoreBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CGUType extends AbstractType
+{
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'label' => false,
+        ));
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars = array_replace($view->vars, array(
+            'label'  => $options['label'],
+        ));
+    }
+    
+    public function getParent()
+    {
+        return 'checkbox';
+    }
+
+    public function getName()
+    {
+        return 'ywc_cgu';
+    }
+}
